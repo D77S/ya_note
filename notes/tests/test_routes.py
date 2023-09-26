855037703172771be23fd25e230d509bb5abf967
Дмитрий <d77s@inbox.ru>
2023-09-26 21:14:29 +0300

    Next

--- notes/tests/test_routes.py
+++ notes/tests/test_routes.py
@@ -73,22 +73,27 @@ class TestRoutes(TestCase):
                     response = self.client.get(url)
                     self.assertEqual(response.status_code, status)
 
-    # def test_redirect_for_anonymous_client(self):
+    def test_redirect_for_anonymous_client(self):
         # Сохраняем адрес страницы логина:
-        # login_url = reverse('users:login')
-        # print('адрес страницы логина: ', login_url)
+        login_url = reverse('users:login')
         # В цикле перебираем имена страниц, с которых ожидаем редирект:
-        # for name in ('news:edit', 'news:delete'):
-        #     with self.subTest(name=name):
-                # Получаем адрес страницы редактирования или удаления комментария:
-        #         url = reverse(name, args=(self.comment.id,))
-        #         print('адрес страницы редактирования или удаления комментария: ', url)
+        for name in (
+            # ('notes:add'),
+            # ('notes:list'),
+            # ('notes:success'),
+            ('notes:edit'),
+            # ('notes:detail'),
+            ('notes:delete')
+        ):
+
+            with self.subTest(name=name):
+                # Получаем адрес страницы редактирования или удаления записи:
+                url = reverse(name, args=(self.Note_test.slug,))
                 # Получаем ожидаемый адрес страницы логина,
                 # на который будет перенаправлен пользователь.
-                # Учитываем, что в адресе будет параметр next, в котором передаётся
+                # Учитываем, что в адресе будет параметр next, в к-м передаётся
                 # адрес страницы, с которой пользователь был переадресован.
-        #         redirect_url = f'{login_url}?next={url}'
-        #         print('ожидаемый адрес страницы логина, на который будет перенаправлен пользователь: ', redirect_url)
-        #         response = self.client.get(url)
+                redirect_url = f'{login_url}?next={url}'
+                response = self.client.get(url)
                 # Проверяем, что редирект приведёт именно на указанную ссылку.
-        #         self.assertRedirects(response, redirect_url)
+                self.assertRedirects(response, redirect_url)
