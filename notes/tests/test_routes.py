a46e8fc03b339132b4073c0eb9cf2ce128bc2277
Dmitry <d77s@inbox.ru>
2023-09-26 13:04:01 +0300

    Next

---
+++ notes/tests/test_routes.py
@@ -0,0 +1,85 @@
+from http import HTTPStatus
+
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+from django.urls import reverse
+
+from news.models import Comment, News
+
+
+User = get_user_model()
+
+
+class TestRoutes(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.news = News.objects.create(title='Заголовок', text='Текст')
+        # Создаём двух пользователей с разными именами:
+        cls.author = User.objects.create(username='Лев Толстой')
+        cls.reader = User.objects.create(username='Читатель простой')
+        cls.comment = Comment.objects.create(
+            news=cls.news,
+            author=cls.author,
+            text='Текст комментария'
+        )
+
+    def test_pages_availability(self):
+        # Создаём набор тестовых данных - кортеж кортежей.
+        # Каждый вложенный кортеж содержит два элемента:
+        # имя пути и позиционные аргументы для функции reverse().
+        urls = (
+            ('news:home', None),
+            ('news:detail', (self.news.id,)),
+            ('users:login', None),
+            ('users:logout', None),
+            ('users:signup', None),
+        )
+
+        # При обращении к страницам редактирования и удаления комментария
+        users_statuses = (
+            (self.author, HTTPStatus.OK),  # автор комментария должен получить ответ OK,
+            (self.reader, HTTPStatus.NOT_FOUND),  # читатель должен получить ответ NOT_FOUND.
+
+        )
+
+        # Итерируемся по внешнему кортежу
+        # и распаковываем содержимое вложенных кортежей:
+        for name, args in urls:
+            with self.subTest(name=name):
+                # Передаём имя и позиционный аргумент в reverse()
+                # и получаем адрес страницы для GET-запроса:
+                url = reverse(name, args=args)
+                response = self.client.get(url)
+                self.assertEqual(response.status_code, HTTPStatus.OK)
+
+        for user, status in users_statuses:
+            # Логиним пользователя в клиенте:
+            self.client.force_login(user)
+            # Для каждой пары "пользователь - ожидаемый ответ"
+            # перебираем имена тестируемых страниц:
+            for name in ('news:edit', 'news:delete'):
+                with self.subTest(user=user, name=name):
+                    url = reverse(name, args=(self.comment.id,))
+                    response = self.client.get(url)
+                    self.assertEqual(response.status_code, status)
+
+    def test_redirect_for_anonymous_client(self):
+        # Сохраняем адрес страницы логина:
+        login_url = reverse('users:login')
+        print('адрес страницы логина: ', login_url)
+        # В цикле перебираем имена страниц, с которых ожидаем редирект:
+        for name in ('news:edit', 'news:delete'):
+            with self.subTest(name=name):
+                # Получаем адрес страницы редактирования или удаления комментария:
+                url = reverse(name, args=(self.comment.id,))
+                print('адрес страницы редактирования или удаления комментария: ', url)
+                # Получаем ожидаемый адрес страницы логина,
+                # на который будет перенаправлен пользователь.
+                # Учитываем, что в адресе будет параметр next, в котором передаётся
+                # адрес страницы, с которой пользователь был переадресован.
+                redirect_url = f'{login_url}?next={url}'
+                print('ожидаемый адрес страницы логина, на который будет перенаправлен пользователь: ', redirect_url)
+                response = self.client.get(url)
+                # Проверяем, что редирект приведёт именно на указанную ссылку.
+                self.assertRedirects(response, redirect_url)
