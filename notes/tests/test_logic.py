5b972049ba42b951ca6864aa6584e5e7db58fd09
Дмитрий <d77s@inbox.ru>
2023-09-29 19:39:52 +0300

    Next

--- notes/tests/test_logic.py
+++ notes/tests/test_logic.py
@@ -4,7 +4,7 @@ from django.contrib.auth import get_user_model
 from django.test import Client, TestCase
 from django.urls import reverse
 
-from notes.forms import NoteForm, WARNING
+from notes.forms import WARNING
 from notes.models import Note
 
 User = get_user_model()
@@ -98,7 +98,7 @@ class TestNoteEditDelete(TestCase):
     возможности редактировать и удалить свою запись,
     невозможности редактировать и удалить чужую запись.
     '''
-    
+
     @classmethod
     def setUpTestData(cls):
         # Создаём автора и его клиент,
